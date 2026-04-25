8297cf684430b37a2d148f68b26782f125efc98f
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-04-25 15:11:56 +0545

    get , del ,post done!

--- crud-api/go.mod
+++ crud-api/go.mod
@@ -1,3 +1,5 @@
 module go-CRUD
 
 go 1.26.2
+
+require github.com/gorilla/mux v1.8.1
--- crud-api/main.go
+++ crud-api/main.go
@@ -1,7 +1,182 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	Id     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Name   string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var film []Movie
+
+func init() {
+	//using apend methods
+	film = append(film, Movie{Id: "0", Isbn: "98456", Name: "Avatar", Author: &Author{FirstName: "Jone", Lastname: "Hero"}})
+
+	film = append(film, Movie{Id: "1", Isbn: "94456", Name: "Ava", Author: &Author{FirstName: "Captain", Lastname: "USA"}})
+
+}
 
 func main() {
+
+	// fmt.Print(film)
+
 	fmt.Println("Create, Read, Update, and Delete")
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", Hello).Methods("GET")
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+
+	r.HandleFunc("/movie/{id}", getMovie).Methods("GET")
+
+	r.HandleFunc("/movie", createMovie).Methods("POST")
+
+	// r.HandleFunc("/updatemovie/{id}", updateMovie).Methods("PUT")
+
+	r.HandleFunc("/movie/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Serving at http://localhost:8000")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+// simple function
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+	w.Write([]byte("\nHellno"))
+}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+
+	// fmt.Println("serving Movies")
+	w.Header().Set("Content-Type", "application/json")
+
+	// seeding with fake value to test
+	json.NewEncoder(w).Encode(film)
+
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Serving a movie")
+
+	// set the header to json only accepts json
+	w.Header().Set("Content-Type", "application/json")
+
+	// htt....com/user?name=".."
+	prams := mux.Vars(r)
+
+	// turning the str parms in int ,don't need when used name to serve movie
+	index, _ := strconv.Atoi(prams["id"])
+
+	// Access by index (with a safety check)
+	if index >= 0 && index < len(film) {
+
+		json.NewEncoder(w).Encode(film[index])
+		return
+
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(map[string]string{
+		"error": "Movie not found",
+		"info":  "Index out of range",
+	})
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Deleted !")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	prams := mux.Vars(r)
+	// id := prams["id"]
+
+	index, _ := strconv.Atoi(prams["id"])
+
+	// for i, Movie_data := range film {
+	// 	if Movie_data.Id == id {
+
+	// 		// Remove
+	// 		film = append(film[:i], film[i+1:]...)
+
+	// 		// Send back the updated data (or the whole film)
+	// 		w.WriteHeader(http.StatusOK)
+	// 		json.NewEncoder(w).Encode(film)
+
+	// 		return
+
+	// 	}
+
+	// }
+
+	if index >= 0 && index < len(film) {
+
+		film = append(film[:index], film[index+1:]...)
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(film)
+		return
+
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(map[string]string{"error": "movie not found"})
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Adding a new movie to DB!")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Invalid movie_data")
+	}
+
+	// {}
+
+	var movie Movie
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode("Invalid JSON data")
+		return
+	}
+
+	if movie.Name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode("Movie title is required")
+		return
+	}
+
+	// rand.Seed(time.Now().UnixNano()) //no seeding required
+
+	movie.Id = strconv.Itoa(rand.Intn(100))
+
+	// library is a slice
+	// var library []movie
+
+	film = append(film, movie)
+
+	fmt.Printf("Adding a new movie: %s! Total: %d\n", movie.Name, len(film))
+	w.WriteHeader(http.StatusCreated) // Use 201 Created for new resources
+	json.NewEncoder(w).Encode(movie)
 }
